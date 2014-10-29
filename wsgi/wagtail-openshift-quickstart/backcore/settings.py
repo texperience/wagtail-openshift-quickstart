cfb45519dc296f8ba6eb662a892355de710c6066
Timo Rieber <dev@timorieber.de>
2014-10-29 20:01:52 +0100

    Enabled media upload and handling

--- wsgi/wagtail-openshift-quickstart/backcore/settings.py
+++ wsgi/wagtail-openshift-quickstart/backcore/settings.py
@@ -18,6 +18,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.join(os.path.dirname(__file__), "../../..")
 
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = "/media/"
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
--- wsgi/wagtail-openshift-quickstart/backcore/urls.py
+++ wsgi/wagtail-openshift-quickstart/backcore/urls.py
@@ -1,10 +1,14 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailsearch.urls import frontend as wagtailsearch_frontend_urls
+
+import os.path
 
 urlpatterns = patterns("",
     url(r"^django/", include(admin.site.urls)),
@@ -14,3 +18,9 @@ urlpatterns = patterns("",
     url(r"^documents/", include(wagtaildocs_urls)),
     url(r"", include(wagtail_urls)),
 )
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns += staticfiles_urlpatterns() # tell gunicorn where static files are in dev mode
+    urlpatterns += static(settings.MEDIA_URL + "images/", document_root=os.path.join(settings.MEDIA_ROOT, "images"))
